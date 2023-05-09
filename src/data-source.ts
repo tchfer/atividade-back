82b11816442194ec6640b25128d55dd171abac05
FFernandoSilva <fdasilva@imaginationmedia.com>
2023-05-09 10:56:55 -0300

    Fix module

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/Tasks"
+import { Tasks } from "./entity/Tasks"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     database: "test",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [Tasks],
     migrations: [],
     subscribers: [],
 })
