5b16c30af0baab1d5112af16384ed03286c8aa55
Rafael Mih <rafaelwsantos@gmail.com>
2023-05-17 21:31:51 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -2,46 +2,45 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
 import bodyParser from "body-parser";
-import cors from 'cors';
+import cors from "cors";
 import routes from "./routes";
 require("dotenv").config();
 
-const config:any = {
-   "type": process.env.DB_DIALECT,
-   "host": process.env.DB_HOST,
-   "port": process.env.DB_PORT,
-   "username": process.env.DB_USER,
-   "password": process.env.DB_PASSWORD,
-   "database": process.env.DB_NAME,
-   "synchronize": true,
-   "ssl": {
-      "rejectUnauthorized": false
-    },
-   "logging": false,
-   "entities": [
-      "./src/entity/*"
-   ],
-   "migrations": [
-      "src/migration/**/*.ts"
-   ],
-   "subscribers": [
-      "src/subscriber/**/*.ts"
-   ],
-   "cli": {
-      "entitiesDir": "src/entity",
-      "migrationsDir": "src/migration",
-      "subscribersDir": "src/subscriber"
-   }
-}
+const config: any = {
+  type: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  extra: {
+    ssl: true,
+  },
+  logging: false,
+  entities: ["./src/entity/*"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber",
+  },
+};
 
-const app = express()
-createConnection(config).then(async connection => { 
-   console.log("TypeORM with MongoDB"); 
-}).catch(error => console.log(error));
-app.use(cors())
-app.use(bodyParser.json())
-app.use(routes)
- 
+const app = express();
+createConnection(config)
+  .then(async (connection) => {
+    console.log("TypeORM with MongoDB");
+  })
+  .catch((error) => console.log(error));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(routes);
+
 app.listen(process.env.PORT || 3333, () => {
-  console.log('Servidor em Execução');
+  console.log("Servidor em Execução");
 });
